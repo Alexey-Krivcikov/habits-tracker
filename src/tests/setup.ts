0b8963fd6f741068eee8f42254053f1a612d2bf2
Aleksey Krivchikov <krivchikovalexei@yandex.ru>
2026-08-27 09:17:38 +0300

    test: add global afterEach cleanup and mock reset in setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,4 +1,6 @@
 import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach, vi } from "vitest";
 
 Object.assign(process.env, {
   NODE_ENV: "test",
@@ -38,4 +40,10 @@ Object.defineProperty(document, "fonts", {
     addEventListener: () => {},
     removeEventListener: () => {},
   },
+});
+
+afterEach(() => {
+  cleanup();
+  vi.clearAllMocks();
+  vi.unstubAllGlobals();
 });
